53094dae6e52d24b37cea4de2a4eaeed60d47737
EduFatou <edufatou@gmail.com>
2024-05-30 08:52:40 +0200

    not finished yet

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
-//RESUELVE TUS EJERCICIOS AQUI
+
+//1
+const getAllBreeds = async()=> {
+    try {
+        const respuesta = await fetch('https://dog.ceo/api/breeds/list/all');
+        const data = await respuesta.json();
+        const razas = Object.keys(data.message);
+        return razas;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+//2
+const getRandomDog = async()=> {
+    try {
+        const respuesta = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await respuesta.json();
+        const imagen = data.message;
+        return imagen;
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+//3
+const getAllImagesByBreed = async()=> {
+    try {
+        const respuesta = await fetch('https://dog.ceo/api/breed/komondor/images');
+        const data = await respuesta.json();
+        const komondor = data.message;
+        return komondor;
+    }catch (error){
+        console.log(error);
+    }
+};
+
+//4
+const getAllImagesByBreed2 = async(breed)=> {
+    try {
+        const respuesta = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+        const data = await respuesta.json();
+        const raza = data.message;
+        return raza;
+    } catch (error){
+        console.log(error);
+    }
+};
+
+//5
+const getGitHubUserProfile = async(username)=> {
+    try {
+        const respuesta = await fetch(`https://api.github.com/users/${username}`);
+        if (respuesta.ok) {
+            const user = await respuesta.json();
+            return user;
+        } else {
+            throw error;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//6
+const printGithubUserProfile = async(username)=> {
+    try {
+        const respuesta = await fetch(`https://api.github.com/users/${username}`);
+        if (respuesta.ok) {
+            const data = await respuesta.json();
+            //no doy para más
+
+        } else {
+            throw error;
+        }
+    } catch (error){
+        console.log(error);
+    }
+};
+
+//7
+getAndPrintGitHubUserProfile(username)
